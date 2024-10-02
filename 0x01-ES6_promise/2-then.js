cd29c3e7136d20aac3c35da0e1642e131891bfa5
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-10-02 20:03:42 +0300

    p

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error(''));
+    }
+  }).then(console.log('Got a response from the API'));
+}
