3487dd48c9d647e125124a7a009de80988b85b85
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-10-05 01:54:36 +0300

    p

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  };
+
+  get code() {
+     return this._code; 
+    };
+
+  get name() { return this._name; };
+
+  set name(val) {
+    if (typeof val !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+    this._name = val;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+        throw new TypeError('Name must be a string');
+    }
+    this._code = value;
+  };
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
