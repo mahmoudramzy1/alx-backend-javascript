26300ebbc51754f695dfd393d76c0919a323ca32
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-10-05 01:41:59 +0300

    p

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,41 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  set students(value) {
+    if (value instanceof Array && value.every((student) => typeof student === 'string')) {
+      this._students = value;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
+}
