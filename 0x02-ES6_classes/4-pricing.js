411cea31213ddfb886e7b6c8906be0d80bc339c2
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-10-05 02:15:21 +0300

    p

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,45 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this.amount;
+  }
+
+  get currency() {
+    return this.currency;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this.currency = value;
+    } else {
+      throw new TypeError('currency must be an instance of Currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullPrice()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
