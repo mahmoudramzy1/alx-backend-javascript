11d5ce1de8f43487db6f8c401a54c9d27790bd44
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-10-06 17:58:53 +0300

    p

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) if (!this.evacuationWarningMessage) throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+
+  get sqft() { return this.sqft; }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._sqft = value;
+  }
+}
